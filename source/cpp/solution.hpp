820e86da82916227086ca9de55d1cd552c06bdb2
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-04-03 20:21:38 +0530

    feat(cpp): 🧩 add C++ header (solution.hpp) with 3Sum skeleton 💀implementation

---
+++ source/cpp/solution.hpp
@@ -0,0 +1,13 @@
+#ifndef SOLUTION_HPP
+#define SOLUTION_HPP
+
+#include <vector>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        return std::vector<std::vector<int>> ();
+    }
+};
+
+#endif
